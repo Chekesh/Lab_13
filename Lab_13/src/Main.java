b0959b7795c883cb6ce0924c8986cd2c56629f41
Chekesh <122931647+Chekesh@users.noreply.github.com>
2023-05-19 02:00:03 +0300

    created

---
+++ Lab_13/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.regex.*;
+public class Main {
+    static void Writefile(String str){
+        try(FileWriter fw = new FileWriter("IpTrue.txt")){
+            for(int i=0; i<str.length();i++){
+                fw.write(str.charAt(i));
+            }
+        }
+        catch(IOException ex){
+            System.out.println(System.out);
+        }
+    }
+    public static void main(String[] args) {
+        String sp = "111.222.233.0";
+        Pattern p = Pattern.compile("((\\d|1?\\d\\d|2([0-4]\\d|5[0-5]))\\.){3}(\\d|1?\\d\\d|2([0-4]\\d|5[0-5]))");
+        Matcher m = p.matcher(sp);
+        if(m.find()){
+            Writefile(sp);
+        }
+        else{
+            Writefile("IP: "+sp+" не верен");
+        }
+    }
+}
